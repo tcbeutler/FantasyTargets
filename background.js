3161cc7097db463a9796cce5899be14b16356934
Thomas Beutler <tcbeutler@Thomass-MacBook-Air.local>
2013-12-08 23:20:27 -0600

    Background script passes URL of playerdata to content script.

--- background.js
+++ background.js
@@ -1,23 +1,24 @@
 console.log('hi');
 // chrome.webRequest.onCompleted.addListener(function(details) {
-// 	console.log('completed' + details);
+//  console.log('completed' + details);
 // }, {urls: ["http://*/*"]}, ["responseHeaders"]);
 chrome.webRequest.onCompleted.addListener(
 function(details) {
-    //if (details.url.substring(0, 23) == "https://www.google.com/") // I know I do not need this
-    //{
-        console.info("URL :" + details.url);
-        FindData("http://www.altavista.com");
-    //}
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {url: "hello"}, function(response) {
+        console.log(response);
+      });
+    });
 },
+
 // filters
 {
     urls: [
-	    "http://games.espn.go.com/ffl/format/playerpop/*",
+        "http://games.espn.go.com/ffl/format/playerpop/*",
     ]
 },
 ["responseHeaders"]);
 
 function FindData(str) {
-	console.log('im here');
+    console.log('im here');
 }
--- contentscript.js
+++ contentscript.js
@@ -3,13 +3,19 @@ $("body").append('Test');
 
 var pnum;
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://espn.go.com/nfl/player/_/id/15168/case-keenum", true);
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-        pnum = $('ul.general-info li.first', xhr.responseText).text().trim().split(' ')[0]
-        $('div.player-name').append('<p>' + pnum + '</p>');
-    	console.log('SUP ' + pnum + ' SON');
-  }
-}
-xhr.send();
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(request.url);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://espn.go.com/nfl/player/_/id/15168/case-keenum", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            pnum = $('ul.general-info li.first', xhr.responseText).text().trim().split(' ')[0];
+            $('div.player-name').append('<p>' + pnum + '</p>');
+          console.log('SUP ' + pnum + ' SON');
+      }
+    };
+    xhr.send();
+    console.log('end of  listener');
+  });

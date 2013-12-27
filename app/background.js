6707266f61781b4413060a10f18ec7be27b3d5b6
Tbomas Beutler <tcbeutler@gmail.com>
2013-12-26 20:53:38 -0500

    Restuctured spacing in background js

--- app/background.js
+++ app/background.js
@@ -1,17 +1,14 @@
-console.log('hi');
 chrome.webRequest.onCompleted.addListener(
-    function(details) {
+  //callback
+  function(details) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {url: details.url}, function(response) {
       });
     });
-},
-
-// filters
-{
-    urls: [
-        "http://games.espn.go.com/ffl/format/playerpop/*",
-    ]
-},
-["responseHeaders"]);
+  },
+  // filters
+  { urls: ["http://games.espn.go.com/ffl/format/playerpop/*"] },
+  //permissions
+  ["responseHeaders"]
+);
 

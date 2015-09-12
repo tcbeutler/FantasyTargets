b7de31b0c810cb559054e87981e178ffc2e4ff38
Tbomas Beutler <tcbeutler@gmail.com>
2015-09-12 11:22:23 -0700

    Trimmed the fat, updated TODO
    Rookie player numbers are broken using ESPN, need a new source

--- README.md
+++ README.md
@@ -7,10 +7,21 @@
 ![Preview](http://i.imgur.com/OnAVQo2.jpg)
 ==============
 
-####Idea log: 
-* Style modifications to tabs above with "videos" etc so they're not so ugly.
+####TODO:
 * Add caching of requsts  
         https://github.com/monsur/jscache  
+        https://github.com/knadh/localstorageDB
+
+* Better player number source (ESPN rookies broken)
+        ESPN rookie data uses inconsistent player numbers
+
+* Pre-draw columns during loading (and remove if fails)
+* Pre-fetch data on page
+* Add options page
+
+####Idea log: 
+* Style modifications to tabs above with "videos" etc so they're not so ugly.
+
 * Move player "more stats" into tabs and name them properly.
 * Show collective injury data  from previous year(P, Q, O, IR designations) Maybe next to week column.
         http://fftoday.com/nfl/13_injury_wk14.html
@@ -21,19 +32,6 @@
 * Add rotoworld news updates!
        http://www.rotoworld.com/content/playersearch.aspx?searchname=gordon%2c+josh&sport=mlb
 
-
-####Possible scrape sites:  
-
-######YAHOO  
------  
-1. http://sports.yahoo.com/nfl/teams/atl/roster/ => $('a[title="Roddy White"]')  
-        Possibly cache this using storage.
-2. http://sports.yahoo.com/nfl/players/7203/ => $('div#mediasportsplayergamelog td.nfl-stat-type-310.targets')  
-3. Injury history - http://www.kffl.com/player/9270/nfl/injury_history/steven-jackson
-4. http://sports.yahoo.com/nfl/stats/bycategory?cat=Receiving&conference=NFL&year=season_2013&timeframe=Week15&sort=32&old_category=Receiving
-
-fftoday.com/stats/players  
-http://www.kffl.com/player/{id}/nfl/utilization  
 
 
 
--- app/contentscript.js
+++ app/contentscript.js
@@ -1,86 +1,26 @@
 var FantasyProsStatsProvider = require('FantasyProsStatsProvider');
+var EspnStatsProvider = require('EspnStatsProvider');
+var dom = require('DomController')();
+var $ = require('jquery');
 
 chrome.runtime.onMessage.addListener(
 
   function(request, sender, sendResponse) {
     var playerName = $('div.player-name').text();
-    var fp = new FantasyProsStatsProvider(playerName);
-    // fp.getStats();
-    addTargets(fp)
+    console.log(request.url)
+    var espnPlayerId = request.url.match(/playerId=(\d+)&/)[1];
+    console.log(espnPlayerId)
 
-    // compare with espn and yahoo
-    var position = $('span[title="Position Eligibility"').text().split(' ')[1];
-    var playerId = request.url.match(/playerId=(\d+)&/)[1];
-    var espn = new espnStatsProvider(playerId)
-    addTargets(espn);
+    new FantasyProsStatsProvider(playerName)
+      .getStats()
+      .then(dom.addTargets, handleError)
 
-    var team = $('span[title="Team"]').text();
-    var playerName = $('div.player-name').text();
-    var yahoo = new yahooStatsProvider(team, playerName);
-    addTargets(yahoo);
+    new EspnStatsProvider(espnPlayerId)
+      .getPlayerNumber()
+      .then(dom.addPlayerNumber, handleError)
+
+    function handleError(err) {
+      console.error('Error', err);
+    }
   }
 );
-
-function addTargets(statProvider) {
-  var start = new Date();
-  var data = statProvider.getStats();
-
-  data.done(function(r) {
-    console.log(r)
-    var took = new Date() - start;
-    console.log(statProvider.name + ' took - ', took);
-    if(!r.targets)
-      return;
-    //targets = adjustForMissingWeeks(r.targets, r.yards, r.receptions);
-    injectCells(2, r.targets);  
-  });
-
-}
-
-function injectCells(index, values) {
-  var tableRows = $('div#tabView0 div#moreStatsView0 div#pcBorder table tbody tr');
-  for (var i = 0; i < tableRows.length; i++) {
-
-    var newCell = tableRows[i].insertCell(index);
-    newCell.setAttribute('width', 100);
-    newCell.setAttribute('align', 'right');
-    //Set class
-    if (i === 0)
-      newCell.setAttribute('class', 'pcTanRight');
-    else
-      newCell.setAttribute('class', i%2 ? 'pcEven' : 'pcOdd');
-    newCell.innerText = values[i] || '-';
-  }
-}
-
-function adjustForMissingWeeks(targets, yards, receptions) {
-  //Fill in blanks for missing games, byes. etc
-  var tableRows = $('div#tabView0 div#moreStatsView0 div#pcBorder table tbody tr');
-  var newTargets = [];
-  var iter = 0;
-  for (var i = 0; i < tableRows.length; i++) {
-    if (cellsEqual(tableRows[i], [1], 'BYE')) {
-      newTargets.push('-');
-    }
-    else if (cellsEqual(tableRows[i], [2, 3, 4, 5], '-')) {
-     newTargets.push('-');
-    }
-    else if (cellsEqual(tableRows[i], [2, 3, 4, 5], '0') && receptions[iter] !== '0' && yards[iter] !== '0') {
-     newTargets.push('0');
-    }
-    else {
-      newTargets.push(targets[iter] || '');
-      iter++;
-    }
-  }  
-  return newTargets;
-  
-}
-
-function cellsEqual(row, cellIndexArray, value) {
-  for (var i = 0; i < cellIndexArray.length; i++) {
-    if (row.cells[cellIndexArray[i]].innerText !== value)
-      return false;
-  }
-  return true;
-}
